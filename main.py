2b55e4c91b6032edb471c31ebb145a55c7471d63
WoollyBeardBear <peterakluth@gmail.com>
2025-03-24 14:39:01 -0600

    added line and point

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
-from window import Window
+from window import *
+from tkinter import Tk, BOTH, Canvas
 
 
 def main():
     win = Window(800, 600)
+    win.draw_line(Line(Point(10, 10), Point(100, 100)), "red")
+    win.draw_line(Line(Point(10, 100), Point(100, 10)), "blue")
+    win.draw_line (Line(Point(500, 10), Point(10, 100)), "green")
     win.wait_for_close()
 
 main()
