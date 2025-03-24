586579d27274f39f22b32410c16bce9cd3df9fac
WoollyBeardBear <peterakluth@gmail.com>
2025-03-24 14:18:47 -0600

    fixed some issues with Canvas

--- window.py
+++ window.py
@@ -2,12 +2,10 @@ from tkinter import Tk, BOTH, Canvas
 
 class Window:
     def __init__(self, width, height):
-        self.width = width
-        self.height = height
         self.__root = Tk()
         self.__root.title("Maze Solver")
-        self.__canvas = Canvas()
-        self.__canvas.pack()
+        self.__canvas = Canvas(self.__root, bg="white", height=height, width=width)
+        self.__canvas.pack(fill=BOTH, expand=1)
         self.running = False
         self.__root.protocol("WM_DELETE_WINDOW", self.close)
 
